6c9838eef893bd4e97ad8a73f297daee7c505cbb
matrix0123456789 <Matrix0123456789@tlen.pl>
2013-11-14 22:03:12 +0100

    Dodanie prowizorycznej obsługi touchpada po HTTP

--- src/HTTP.java
+++ src/HTTP.java
@@ -3,9 +3,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import org.json.*;
+
+import com.example.socketclient.TCP_Data;
+import com.example.socketclient.TCP_Data.touchedTYPE;
+import com.example.socketclient.TCP_Data.typ;
 
 public class HTTP {
-static void polaczenie(InputStream is, Socket soc) throws IOException
+static TCP_Data polaczenie(InputStream is, Socket soc) throws IOException
 {
 	byte liczbaNowychLinii=0;
 	String wyj="";
@@ -30,19 +35,49 @@ int n = is.read();
 
 		break;
 	}}
-	if(wyj.indexOf("/")==0)
+	System.out.print(wyj);
+	if(wyj.indexOf("/?")==0)
+	{
+		String klasaString=wyj.substring(2, wyj.indexOf(" HTTP"));
+		klasaString=klasaString.replaceAll("%22", "\"");
+		JSONObject klasaObjekt=new JSONObject(klasaString);
+		TCP_Data ret=new TCP_Data();
+
+		ret.touchpadX=klasaObjekt.getInt("touchpadX");
+		ret.touchpadY=klasaObjekt.getInt("touchpadY");
+		ret.type=typ.values()[klasaObjekt.getInt("type")];
+		ret.mouse=touchedTYPE.values()[klasaObjekt.getInt("mouse")];
+		
+		OutputStream os =soc.getOutputStream();
+		
+		os.close();
+		is.close();
+		return ret;
+	}
+	else if(wyj.indexOf("/")==0)
 	{
 		OutputStream os =soc.getOutputStream();
 		String wysylanie="HTTP/1.1 200 OK\r\nServer: PilotPC\r\nContent-Type: application/xhtml+xml\r\n\r\n"
-				+"<?xml version=\"1.0\" encoding=\"UTF-8\"?><html xmlns=\"http://www.w3.org/1999/xhtml\">	<head>		<title>PilotPC</title></head>"
+				+"<?xml version=\"1.0\" encoding=\"UTF-8\"?><html xmlns=\"http://www.w3.org/1999/xhtml\">	<head>		<title>PilotPC</title>"
 				+"<style>#menu{position: absolute;bottom: 0;background: gray;margin: 0;left: 0;width: 100%;}#menu li{display:inline;}.karta{position:absolute;left:0;top:0;width:100%;bottom:20px;display:none;}</style>"
-				+"<body><div class=\"karta\" id=\"gamepad\"></div><div class=\"karta\" id=\"pilot\"></div><div class=\"karta\" id=\"klawiatura\"></div><div class=\"karta\" style=\"display:block\" ontouchmove=\"alert(event)\" id=\"touchpad\"></div>"
+				+"<script> var touchpad=new Object();touchpad.mPreviousX=0; touchpad.mPreviousY=0;touchpad.oldX =0;touchpad.oldY=0;touchpad.longClicked = false;touchpad.returnState = true;"
+				+"touchpad.onTouchMove=function(e){var dx = event.clientX - this.mPreviousX;var dy = event.clientY - this.mPreviousY;"
+				+"var data=new TCP_Data();data.mouse = TCP_Data.touchedTYPE.NORMAL;data.touchpadX = dx;data.touchpadY =dy;data.type=TCP_Data.typ.TOUCHPAD;send(data);};"
+				+"function send(data){"
+				+ "var socket=new XMLHttpRequest();"
+				+ "socket.open('get', '?'+JSON.stringify(data));"
+				+ "socket.send();"
+				+ "}"
+				+ "function TCP_Data(){this.touchpadX=0;this.touchpadY=0;};"
+				+ "TCP_Data.touchedTYPE={NORMAL:0, LONG:1, UP:2, SCROLL:3, LPM:4, PPM:5};"
+				+ "TCP_Data.typ={GAMEPAD:0, PILOT:1, KEYBOARD:2, TOUCHPAD:3};</script>"
+				+"</head><body><div class=\"karta\" id=\"gamepad\"></div><div class=\"karta\" id=\"pilot\"></div><div class=\"karta\" id=\"klawiatura\"></div><div class=\"karta\" style=\"display:block\" ontouchmove=\"touchpad.onTouchMove(event)\" onmousemove=\"touchpad.onTouchMove(event)\" id=\"touchpad\"></div>"
 				+"<ul id=\"menu\"><li><img/></li><li><img title=\"gamepad\"/></li><li><img/></li><li><img title=\"touchpad\"/></li></ul>Aplikacja w trakcie pisania :)</body></html>";
 		os.write(wysylanie.getBytes());
 		os.close();
 		is.close();
 	}
 	//else
-			System.out.print(wyj);
+			return null;
 }
 }
--- src/PolaczenieWatek.java
+++ src/PolaczenieWatek.java
@@ -10,9 +10,9 @@ public class PolaczenieWatek
     extends Thread {
 	public ServerSocket socServ;
 	Socket soc;
-    public void run() {
-    	MouseRobot mouse = new MouseRobot();  //klasa do sterowania myszka
     	Pilot pilot = new Pilot();
+    	MouseRobot mouse = new MouseRobot();
+    public void run() {  //klasa do sterowania myszka
     	while(true){	
     		
 		
@@ -31,82 +31,88 @@ public class PolaczenieWatek
 					break;
 				}
 			}
-			is = soc.getInputStream();
+          		is = soc.getInputStream();
+			try{
 			  ObjectInputStream in = new ObjectInputStream(soc.getInputStream());	
-          		TCP_Data data;
-          		
-          		while(true)
-          		{
-					data = (TCP_Data) in.readObject();
+			  while(true)
+        		{
 					
-						if(data.type == TCP_Data.typ.TOUCHPAD )
-						{							
-							switch(data.mouse)
-							{
-							case LPM:
-							{
-							
-								mouse.LPM();
-								
-								break;
-							}
-							case PPM:
-							{
-								
-								mouse.PPM();
-								break;
-							}
-							case NORMAL:
-							{
-								mouse.move(data.touchpadX, data.touchpadY); //ruszanie myszka
-								break;
-							}
-							case LONG:
-							{
-								// TODO zmiana "algorytmu" ruszania z LPM
-								mouse.move(true, data.touchpadX, data.touchpadY); //ruszanie ze wcisnietym LPM
-								break;
-							}
-							case UP:
-							{
-								mouse.up(); //podniesienie LPM
-								break;
-							}
-							case SCROLL:
-							{
-								mouse.scroll(data.touchpadY);
-								break;
-							}
-							}
-								 								
-						}
-						else if(data.type == TCP_Data.typ.PILOT )
-						{
-							pilot.click(data);
-							System.out.println("pilot");
-						}
-						data.clean();//czyszczenie zmiennych w TCP_Data
-					
-          		}
-				
-					
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				  TCP_Data data = (TCP_Data) in.readObject();
+					wykonuj(data);
+        		}
+			}
+			  catch(Exception e)
+				{
+					try {
+						TCP_Data data=HTTP.polaczenie(is, soc);
+						if(data!=null)
+							wykonuj(data);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				}
+			  }catch(Exception e)
+			  {
+					System.out.print("Błąd, rozłączono. \n\r\r\n");
+				  
+			  }
+					
+				 
 		
 			
 			
 			
-		catch(Exception e)
-		{
-			try {
-				HTTP.polaczenie(is, soc);
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+		
+    	}}
+    	void wykonuj(TCP_Data data)
+    	{
+    		if(data.type == TCP_Data.typ.TOUCHPAD )
+			{							
+				switch(data.mouse)
+				{
+				case LPM:
+				{
+				
+					mouse.LPM();
+					
+					break;
+				}
+				case PPM:
+				{
+					
+					mouse.PPM();
+					break;
+				}
+				case NORMAL:
+				{
+					mouse.move(data.touchpadX, data.touchpadY); //ruszanie myszka
+					break;
+				}
+				case LONG:
+				{
+					// TODO zmiana "algorytmu" ruszania z LPM
+					mouse.move(true, data.touchpadX, data.touchpadY); //ruszanie ze wcisnietym LPM
+					break;
+				}
+				case UP:
+				{
+					mouse.up(); //podniesienie LPM
+					break;
+				}
+				case SCROLL:
+				{
+					mouse.scroll(data.touchpadY);
+					break;
+				}
+				}
+					 								
 			}
-		}
-			System.out.print("Rozłączono \n\r\r\n");}
-    }
+			else if(data.type == TCP_Data.typ.PILOT )
+			{
+				pilot.click(data);
+				System.out.println("pilot");
+			}
+			data.clean();//czyszczenie zmiennych w TCP_Data
+    	}
 }
