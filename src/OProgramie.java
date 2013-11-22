b170dd122bd7ea7c69d286c3ff6d6323cf92a421
matrix0123456789 <Matrix0123456789@tlen.pl>
2013-11-23 00:16:35 +0100

    Wyświetlanie wersji

--- src/OProgramie.java
+++ src/OProgramie.java
@@ -16,7 +16,7 @@ public OProgramie()
 {
 	setSize(300,300);
 	setTitle("O Programie PilotPC");
-	JLabel tresc = new JLabel("<html><h1>PilotPC-PC-Java</h1><p>Programiści:<ul><li>FranQy</li><li>Matrix0123456789</li></ul></p></html>");
+	JLabel tresc = new JLabel("<html><h1>PilotPC-PC-Java</h1><p>wersja "+Program.wersja+"</p><p>Programiści:<ul><li>FranQy</li><li>Matrix0123456789</li></ul></p></html>");
 	add(tresc);
 	setVisible(true);
 }
