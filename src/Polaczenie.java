34970e5dfee8556f5e8dc1a7a9c14d7c4a18af53
matrix0123456789 <Matrix0123456789@tlen.pl>
2013-12-06 20:28:28 +0100

    Zmiana portu

--- src/Polaczenie.java
+++ src/Polaczenie.java
@@ -8,7 +8,7 @@ import java.net.ServerSocket;
 public class Polaczenie {
 	ServerSocket socServ;
 	Robot robot;
-	int port=12345;
+	int port=8753;
 	public static HttpPolaczenie[] polaczeniaHttp=new HttpPolaczenie[100];
 	public static boolean nasluchiwanie=false;
 	public static PolaczenieWatek[] watki= new PolaczenieWatek[100];
@@ -29,6 +29,18 @@ public Polaczenie()
 						break;
 					}
 				}
+				ServerSocket socServ2=new ServerSocket(12345);
+				nasluchiwanie=true;
+						System.out.print("Nasłuchiwanie na porcie 12345\r\n");
+						for(byte i=0;i<100;i++)//Otwiera max 100 połączeń, zapisuje je w tablicy
+						{
+							if(watki[i]==null){
+								watki[i]=new PolaczenieWatek();
+								watki[i].socServ=socServ2;
+								watki[i].start();
+								break;
+							}
+						}
 
 	} catch (BindException e) {
 		System.out.print("Błąd, port "+port+"zajęty\r\n");
