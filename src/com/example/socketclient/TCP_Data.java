a230b04ad16d8fb3566cee1950eeb76e33010d5e
matrix0123456789 <Matrix0123456789@tlen.pl>
2013-12-28 11:35:50 +0100

    Zmiana TCP_Data

--- src/com/example/socketclient/TCP_Data.java
+++ src/com/example/socketclient/TCP_Data.java
@@ -1,9 +1,6 @@
 package com.example.socketclient;
 
-
-
 import java.io.Serializable;
-import java.util.Hashtable;
 
 /**
  * Created by franqy on 29.10.13.
@@ -13,7 +10,9 @@ public class TCP_Data implements Serializable {
 
 	public enum typ{GAMEPAD, PILOT, KEYBOARD, TOUCHPAD};
 	  public enum  pilotButton{OFF, MUSIC, MULTIMEDIA, PLAYPAUSE, PERV, NEXT, STOP, EXIT, BACK, VOLDOWN, VOLUP, MUTE,
-	    UP, DOWN, RIGHT, LEFT, RETTURN};
+	    UP, DOWN, RIGHT, LEFT, RETTURN,
+          REWIND,//Przewijanie do tyłu
+          FORWARD};//przewijanie do przodu
 
 	  public enum touchedTYPE {NORMAL, LONG, UP, SCROLL, LPM, PPM};
 
@@ -22,7 +21,7 @@ public class TCP_Data implements Serializable {
 	    public typ type;
 	    public pilotButton button;
 	    public touchedTYPE mouse;
-    public int key;//kalwiatura
+        public int key;//kalwiatura
 	    public void clean()
 	    {
 	        touchpadX = 0;
