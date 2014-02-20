27399cbf48cb4d5c3d7af4adcdd9597ba2f3060f
matrix0123456789 <Matrix0123456789@tlen.pl>
2014-02-20 23:17:10 +0100

    Wersja 0.2.2

--- src/Program.java
+++ src/Program.java
@@ -11,7 +11,7 @@ public class Program {
     static Polaczenie polaczenia;
     static TypWyswietlania wyswietlanie;
     public static Ustawienia ustawienia = Ustawienia.importuj();
-    static public String wersja = "0.2.1";
+    static public String wersja = "0.2.2";
     static public Robot robot;
     static public TrayIcon trayIcon;
     public static void main(String[] args) throws AWTException {
