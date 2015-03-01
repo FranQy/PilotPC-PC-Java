e08c7b954c1338dc299ed258b13bc7b430c3463e
matrix0123456789 <Matrix0123456789@tlen.pl>
2015-03-01 20:03:40 +0100

    Debugowanie
    Wysyła na serwer inffo o bledzie

--- src/Debugowanie.java
+++ src/Debugowanie.java
@@ -1,8 +1,9 @@
 import org.omg.CORBA.Environment;
+import sun.misc.BASE64Encoder;
 
 import javax.tools.JavaCompiler;
-import java.io.FileWriter;
-import java.io.InputStream;
+import java.io.*;
+import java.net.Socket;
 import java.net.URL;
 import java.util.Date;
 
@@ -16,18 +17,56 @@ public class Debugowanie {
     {
         if (Program.debug)
             e.printStackTrace();
+        String stack="";
+        ByteArrayOutputStream s=new ByteArrayOutputStream();
+        e.printStackTrace(new PrintWriter(s));
+
         String komunikat=(new Date()).toString()+"Błąd w programie PilotPC wersja "+ Program.wersja+"\r\n" +
                 "System:"+ System.getProperty("os.name")+"\r\n" +
                 System.getProperties().toString()+"\r\n" +
             "\r\n"+
-                e.toString()+"\r\n\r\n";
+                e.toString()+"\r\n\r\n"+
+                s.toString()+"\r\n\r\n";
         try{
             FileWriter plik=new FileWriter("error.txt");
             plik.write(komunikat);
+            plik.close();
+        }
+        catch(Throwable ee)
+        {}
+        try{
+
+
+            Socket soc=new Socket("jaebestudio.tk", 80);
+            OutputStream str= soc.getOutputStream();
+            BASE64Encoder en=new BASE64Encoder();
+
+            String base=en.encode(komunikat.getBytes());
+            base=base.replaceAll("\r", "").replaceAll("\n", "");
+            str.write(("POST /debug.php?p=pilotpc HTTP/1.1\r\nHost: jaebestudio.tk\r\n" +
+                    "User-Agent: Mozilla/4.0\r\n" +
+                    "Content-Length: " + (base.length() + 2) + "\r\nContent-Type: application/x-www-form-urlencoded\r\n" +
+                    "Connection: keep-alive\r\n\r\nt=" + base).getBytes());
+            str.flush();
+
+            str.close();
+            soc.close();
+
         }catch(Throwable ee){}
-        try{URL u = new URL("http://jaebestudio.tk/debug.php?t="+komunikat);
-            InputStream is = u.openStream();
-            is.close();}catch(Throwable ee){}
+        try{
+
+
+            Socket soc=new Socket("jaebestudio.tk", 80);
+            OutputStream str= soc.getOutputStream();
+            String base=new sun.misc.BASE64Encoder().encode(komunikat.getBytes());
+            str.write(("POST /debug.php?p=pilotpc HTTP/1.1\r\nHost: jaebestudio.tk\r\nContent-Type: application/x-www-form-urlencoded\r\n" +
+                    "Connection: keep-alive\r\n\r\nt="+ base+"\r\n\r\n").getBytes());
+            str.flush();
+
+            str.close();
+            soc.close();
+
+        }catch(Throwable ee){}
 
     }
 }
