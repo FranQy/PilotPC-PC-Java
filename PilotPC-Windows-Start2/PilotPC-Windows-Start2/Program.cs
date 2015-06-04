41b9399561c4c986a66eba7de9e4e1511ee48b70
matrix0123456789 <Matrix0123456789@tlen.pl>
2015-06-04 19:11:33 +0200

    Poprawki w starterze windows

--- PilotPC-Windows-Start2/PilotPC-Windows-Start2/Program.cs
+++ PilotPC-Windows-Start2/PilotPC-Windows-Start2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
@@ -41,8 +42,11 @@ namespace PilotPC_Windows_Start2
                             pliki = new FileInfo(info.WorkingDirectory + "\\java\\pilotpc.jar");
                             if (pliki.Exists)
                                 arg += "\"" + pliki.FullName + "\"";
-
+                            else
+                            {
+                                
                             MessageBox.Show("Brak pliku pilotpc.jar", "Jaebe PilotPC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
                         }
                     }
                 }
@@ -58,6 +62,7 @@ namespace PilotPC_Windows_Start2
                         arg += " " + args[i];
 
                 }
+                var zmienneŚrodowiskowe=Environment.GetEnvironmentVariables();
                 if (okno)
                     arg += " -o";
                 if ((new FileInfo(plik)).Exists)
@@ -66,12 +71,53 @@ namespace PilotPC_Windows_Start2
                     info.FileName = (new FileInfo(Environment.GetFolderPath(Environment.SpecialFolder.System) + "\\" + plik)).FullName;
                 else if ((new FileInfo(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles) + "\\" + plik)).Exists)
                     info.FileName = (new FileInfo(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles) + "\\" + plik)).FullName;
+                else if (zmienneŚrodowiskowe.Contains("JAVA_PATH") && (new FileInfo(zmienneŚrodowiskowe["JAVA_PATH"] + "\\" + plik)).Exists)
+                    info.FileName = (new FileInfo(zmienneŚrodowiskowe["JAVA_PATH"] + "\\" + plik)).FullName;
                 else
+                {
                     info.FileName = plik;
-                // MessageBox.Show("Nie znaleziono javy", "Jaebe PilotPC", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                info.Arguments = arg;
 
-                var proc = Process.Start(info);
+                    if ((new DirectoryInfo(@"C:\Program Files (x86)\Java")).Exists)
+                    {
+                        var programFilesx86 = (new DirectoryInfo(@"C:\Program Files (x86)\Java")).GetDirectories();
+                        foreach (var x in programFilesx86)
+                        {
+                            var programFilesPlik = new FileInfo(x.FullName + "\\bin\\" + plik);
+                            if (programFilesPlik.Exists)
+                                info.FileName = programFilesPlik.FullName;
+                        }
+                    }
+                    if ((new DirectoryInfo(@"C:\Program Files\Java")).Exists)
+                    {
+                        var programFiles = (new DirectoryInfo(@"C:\Program Files\Java")).GetDirectories();
+                        foreach (var x in programFiles)
+                        {
+                            var programFilesPlik = new FileInfo(x.FullName + "\\bin\\" + plik);
+                            if (programFilesPlik.Exists)
+                                info.FileName = programFilesPlik.FullName;
+                        }
+                    }
+
+                            
+                }
+                // 
+                info.Arguments = arg;
+                try
+                {
+                    var proc = Process.Start(info);
+                }
+                catch(Win32Exception)
+                {
+                    try
+                    {
+                        info.FileName = pliki.FullName;
+                        var proc = Process.Start(info);
+                    }
+                    catch (Win32Exception)
+                    {
+                        MessageBox.Show("Nie znaleziono javy", "Jaebe PilotPC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
 
                 // Application.Run(new Form1());
             }catch(Exception e)
